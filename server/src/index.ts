b15b4fecc8a8a1e4c58d04aa2bbf10dd8bdcbaaf
Chuck Dries <chuck@chuckdries.com>
2021-08-31 18:28:10 -0700

    add more dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hey there')
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import sqlite from 'sqlite3'
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.send("hello world")
+})
+
+app.listen(3000)
